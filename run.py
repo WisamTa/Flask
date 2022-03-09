d2cff882a9c23d439b4b84964d00891eb0be1ec7
WisamTa <wisamtaqaly@gmail.com>
2022-03-09 16:47:54 +0000

    Styling our templates

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-
 import os
 from flask import Flask, render_template
 
@@ -19,6 +18,11 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
